d6dac46a88c00a0f28351730742ab7c59cc46f0d
WesleiRodriguesSouza <souzawesleirodrigues@gmail.com>
2026-02-17 19:46:17 -0300

    feat: adicionando estrutura basica do spring boot

---
+++ backend/src/test/java/com/example/primeiroBackEndTeste/PrimeiroBackEndTesteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.primeiroBackEndTeste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrimeiroBackEndTesteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
